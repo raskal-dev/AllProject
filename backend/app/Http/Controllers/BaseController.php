9149a690315f7ad461d46c43c0a760406bf962a2
raskal-dev <kalvinrasamyzo@gmail.com>
2024-07-26 08:54:18 +0300

    Base Controller

---
+++ backend/app/Http/Controllers/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function sendResponse($data = [], $message = "Success Operation !", $code = 200)
+    {
+        return response([
+            'data' => $data,
+            'message' => $message
+        ], $code);
+    }
+}
